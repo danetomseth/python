e972cc8ba17fb097b9e1b2e8c141c5d03ccbc787
Dane Tomseth <danetomseth@gmail.com>
2017-02-07 16:54:39 -0500

    screen manager and tabs

---
+++ kivyplay/controlTest.py
@@ -0,0 +1,5 @@
+import time
+
+
+def slide():
+    print("IN CONTROL")
--- kivyplay/main.py
+++ kivyplay/main.py
@@ -1,4 +1,5 @@
 from kivy.uix.accordion import Accordion, AccordionItem
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.widget import Widget
 from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty, StringProperty
 from kivy.uix.layout import Layout
@@ -7,120 +8,46 @@ from kivy.uix.label import Label
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 import time
+# import stepperControl as stepper
+import controlTest as stepper
+import motorsPage
+# import temp
 
-class SlideTab(BoxLayout):
-    def __init__(self, **kwargs):
-        super(SlideTab, self).__init__(**kwargs)
-        self.title = "SLIDE"
-        self.set_speed = 1000
-        self.steps_programmed = 64000
-        self.set_direction = 'LEFT'
+ 
 
-        self._set_speed = 'Speed: ' + str(self.set_speed)
-        self._steps_programmed = 'Steps: ' + str(self.steps_programmed)
-        self._set_direction = 'Direction: ' + self.set_direction
-    
-    def control(self):
-        print("Joystick")
+# class MotorPage(Accordion):
+#     slide_tab = ObjectProperty(None)
+#     pan_tab = ObjectProperty(None)
+#     tilt_tab = ObjectProperty(None)
 
-    def find_home(self):
-        print("HOMEEEE")
+class MotorsScreen(Screen):
 
-    def ramp_test(self):
-        print("Ramp")
-
-    def adjust_speed(self, dir_mod):
-        if dir_mod < 0:
-            self.set_speed -= 100
-        else:
-            self.set_speed += 100 
-        self.update_labels()
-
-
-    def update_labels(self):
-        self.ids.set_speed_label.text = 'Speed: ' + str(self.set_speed)
-        self.ids.steps_programmed_label.text = 'Steps: ' + str(self.steps_programmed)
-        self.ids.set_direction_label.text = 'Direction: ' + self.set_direction 
-
-class PanTab(BoxLayout):
-    def __init__(self, **kwargs):
-        super(PanTab, self).__init__(**kwargs)
-        self.title = "PAN"
-        self.set_speed = 1000
-        self.steps_programmed = 64000
-        self.set_direction = 'LEFT'
-
-        self._set_speed = 'Speed: ' + str(self.set_speed)
-        self._steps_programmed = 'Steps: ' + str(self.steps_programmed)
-        self._set_direction = 'Direction: ' + self.set_direction
-    
-    def control(self):
-        print("Joystick")
-
-    def find_home(self):
-        print("HOMEEEE")
-
-    def ramp_test(self):
-        print("Ramp")
-
-    def adjust_speed(self, dir_mod):
-        if dir_mod < 0:
-            self.set_speed -= 100
-        else:
-            self.set_speed += 100 
-        self.update_labels()
-
-
-    def update_labels(self):
-        self.ids.set_speed_label.text = 'Speed: ' + str(self.set_speed)
-        self.ids.steps_programmed_label.text = 'Steps: ' + str(self.steps_programmed)
-        self.ids.set_direction_label.text = 'Direction: ' + self.set_direction 
-
-class TiltTab(BoxLayout):
-    def __init__(self, **kwargs):
-        super(TiltTab, self).__init__(**kwargs)
-        self.title = "TILT"
-        self.set_speed = 1000
-        self.steps_programmed = 64000
-        self.set_direction = 'LEFT'
-
-        self._set_speed = 'Speed: ' + str(self.set_speed)
-        self._steps_programmed = 'Steps: ' + str(self.steps_programmed)
-        self._set_direction = 'Direction: ' + self.set_direction
-    
-    def control(self):
-        print("Joystick")
-
-    def find_home(self):
-        print("HOMEEEE")
-
-    def ramp_test(self):
-        print("Ramp")
-
-    def adjust_speed(self, dir_mod):
-        if dir_mod < 0:
-            self.set_speed -= 100
-        else:
-            self.set_speed += 100 
-        self.update_labels()
-
-
-    def update_labels(self):
-        self.ids.set_speed_label.text = 'Speed: ' + str(self.set_speed)
-        self.ids.steps_programmed_label.text = 'Steps: ' + str(self.steps_programmed)
-        self.ids.set_direction_label.text = 'Direction: ' + self.set_direction 
-
-class PageView(Accordion):
     slide_tab = ObjectProperty(None)
     pan_tab = ObjectProperty(None)
     tilt_tab = ObjectProperty(None)
-    
 
-class TemplateApp(App):
+    def __init__(self, **kwargs):
+        super(MotorsScreen, self).__init__(**kwargs)
+
+class HomeScreen(Screen):
+    def __init__(self, **kwargs):
+        super(HomeScreen, self).__init__(**kwargs)
+
+    def display(self, val):
+        print("Pressed: " + val)
+
+
+
+class Manager(ScreenManager):
+    home_screen = ObjectProperty(None)
+    motors_screen = ObjectProperty(None)
+
+
+class PilapseApp(App):
     
     def build(self):
-        return PageView()
+        return Manager()
 
 
 if __name__ == '__main__':
-    TemplateApp().run()
+    PilapseApp().run()
